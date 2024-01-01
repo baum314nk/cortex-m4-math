7483c8e9f87ede723b54336df827cea10c846ad7
baum314nk <niklas.baumbach314@gmail.com>
2024-01-01 22:49:04 +0100

    Getting everything running on Cortex M4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
-mod matrix;
+#![no_std]
+#![no_main]
+#![allow(unused)]
+// use matrix::Matrix;
 
-use matrix::Matrix;
+use core::mem::size_of;
 
-fn main() {
-    let v = Matrix::new([[1.0], [3.0], [2.0]]);
-    println!("{}\n", v);
+// mod matrix;
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+
+macro_rules! println_u32 {
+    ($name:expr, $addr:expr) => {
+        let addr = $addr as u32;
+        let addr_high = (addr >> 16) as u16;
+        let addr_low = addr as u16;
+        cortex_m_semihosting::export::hstdout_fmt(format_args!(
+            concat!($name, ": 0x{:04X} {:04X}\n"),
+            addr_high, addr_low
+        ))
+    };
+}
+
+const FIRST_ADDRESS: *const i32 = 0x2000_0000 as *const i32;
+const LAST_ADDRESS: *const i32 = 0x2001_FFFC as *const i32;
+
+#[entry]
+unsafe fn main() -> ! {
+    let mut a: u32 = 3;
+    let ptr_a = core::ptr::from_mut(&mut a);
+    hprintln!("a = {}", a);
+    println_u32!("ptr", ptr_a);
+    // let b = 19;
+    core::ptr::write(ptr_a, (ptr_a.read() as u32) << 2);
+    hprintln!("a = {}", ptr_a.read());
+    println_u32!("ptr", ptr_a);
+
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
 }
